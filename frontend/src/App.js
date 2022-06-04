04428c62eadfa4ae63054e35627e05969277f22a
ultragrace <udechukwuakachukwu.com>
2022-06-04 01:57:43 +0100

    Just created the home page and rendered the products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,38 @@
-import logo from './logo.svg';
-import './index.css';
+import logo from './logo.svg'
+import './index.css'
+import data from './data'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">webuy</a>
+    <div className=''>
+      <header className='header'>
+        <a href='/'>webuy</a>
       </header>
+      <main>
+        <h1>Featured Items</h1>
+        <div className='products'>
+          {data.products.map(product => (
+            <div key={product.slug} className='product'>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.images} alt={product.slug} />
+              </a>
+              <div className='product_info'>
+                <a href={`/product/${product.slug}`}>
+                  <h4>{product.name}</h4>
+                </a>
+                {/* <p> {product.category} </p> */}
+                <p>
+                  {' '}
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
