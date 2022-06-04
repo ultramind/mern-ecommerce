8eb60c6da4fbcacecf8be0b014fa44052c6bc10a
ultragrace <udechukwuakachukwu.com>
2022-06-04 08:25:07 +0100

    create backend and setting up the server

---
+++ backend/data.js
@@ -0,0 +1,66 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike slim shirt',
+      category: 'Shirts',
+      images: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 23,
+      description: 'High quality shirt'
+    },
+    {
+      name: 'Adidas Fit Pants',
+      slug: 'adidas-fit-pants',
+      category: 'Pants',
+      images: '/images/p2.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Puma',
+      rating: 4,
+      numReviews: 13,
+      description: 'High quality shirt'
+    },
+    {
+      name: 'Plain Trousers',
+      slug: 'plain-trousers',
+      category: 'Trousers',
+      images: '/images/p3.jpg',
+      price: 200,
+      countInStock: 8,
+      brand: 'Boss',
+      rating: 4.3,
+      numReviews: 10,
+      description: 'High quality shirt'
+    },
+    {
+      name: 'Kelvin Underwears',
+      slug: 'kelvin-underwears',
+      category: 'Underwears',
+      images: '/images/d1.jpg',
+      price: 120,
+      countInStock: 19,
+      brand: 'Kelvin',
+      rating: 4.4,
+      numReviews: 14,
+      description: 'High quality shirt'
+    },
+    {
+      name: 'BYC Pollo',
+      slug: 'byc-pollo',
+      category: 'pollo',
+      images: '/images/d2.jpg',
+      price: 310,
+      countInStock: 9,
+      brand: 'BYC',
+      rating: 4.7,
+      numReviews: 42,
+      description: 'High quality shirt'
+    },
+  ]
+}
+
+export default data;
